7c9fe1435c8473e5b9c6eccc91000124559347b3
Budi Setiawan <budi.setiawan112001@gmail.com>
2022-06-01 12:05:13 +0700

    Add Services

---
+++ app/src/main/java/com/catpedigree/capstone/catpedigreebase/presentation/ui/services/ServicesViewModel.kt
@@ -0,0 +1,16 @@
+package com.catpedigree.capstone.catpedigreebase.presentation.ui.services
+
+import androidx.lifecycle.*
+import com.catpedigree.capstone.catpedigreebase.data.local.repository.UserRepository
+import com.catpedigree.capstone.catpedigreebase.data.local.repository.VeterinaryRepository
+
+class ServicesViewModel(userRepository: UserRepository) : ViewModel() {
+
+    val userItem = userRepository.userItems.asLiveData()
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _errorMessage = MutableLiveData<String>()
+    val errorMessage: LiveData<String> = _errorMessage
+}
