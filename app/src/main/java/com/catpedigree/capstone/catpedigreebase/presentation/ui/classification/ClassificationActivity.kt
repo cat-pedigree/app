a38886024b531cc7b792c31498a1da9c7f56e9bd
Budi Setiawan <budi.setiawan112001@gmail.com>
2022-06-02 14:39:25 +0700

    Classification Tflite

---
+++ app/src/main/java/com/catpedigree/capstone/catpedigreebase/presentation/ui/classification/ClassificationActivity.kt
@@ -0,0 +1,148 @@
+package com.catpedigree.capstone.catpedigreebase.presentation.ui.classification
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.catpedigree.capstone.catpedigreebase.R
+import com.catpedigree.capstone.catpedigreebase.databinding.ActivityClassificationBinding
+import com.catpedigree.capstone.catpedigreebase.ml.Model1
+import com.catpedigree.capstone.catpedigreebase.presentation.ui.main.MainActivity
+import com.catpedigree.capstone.catpedigreebase.utils.CameraUtils
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+
+class ClassificationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityClassificationBinding
+    private var imageCapture: ImageCapture?= null
+    private var cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityClassificationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cameraCapture.setOnClickListener { takePhoto() }
+        binding.cameraRotate.setOnClickListener {
+            cameraSelector = if (cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) CameraSelector.DEFAULT_FRONT_CAMERA
+            else CameraSelector.DEFAULT_BACK_CAMERA
+            startCamera()
+        }
+
+        binding.cameraGallery.setOnClickListener {
+            startGallery()
+        }
+
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        hideSystemUI()
+        startCamera()
+    }
+
+    private fun takePhoto() {
+        imageCapture ?: return
+
+        val photoFile = CameraUtils.createFile(application)
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        imageCapture!!.takePicture(outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val intent = Intent().apply {
+                        putExtra("picture", photoFile)
+                        putExtra(
+                            "isBackCamera",
+                            cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA
+                        )
+                    }
+                    setResult(MainActivity.CAMERA_X_RESULT, intent)
+                    finish()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Toast.makeText(
+                        this@ClassificationActivity,
+                        getString(R.string.failed_to_take_picture),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+            } catch (e: Exception) {
+                Toast.makeText(
+                    this@ClassificationActivity,
+                    getString(R.string.failed_to_start_camera),
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun startGallery() {
+        val intent = Intent().apply {
+            action = Intent.ACTION_GET_CONTENT
+            type = "image/*"
+        }
+        launcherIntentGallery.launch(Intent.createChooser(intent, "Choose a picture!"))
+    }
+
+    private val launcherIntentGallery =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                val photoFile = CameraUtils.uriToFile(it.data?.data as Uri, this)
+                val intent = Intent().apply {
+                    putExtra("photoFile", photoFile)
+                }
+                setResult(RESULT_OK, intent)
+                finish()
+            }
+        }
+
+
+    private fun hideSystemUI() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+}
