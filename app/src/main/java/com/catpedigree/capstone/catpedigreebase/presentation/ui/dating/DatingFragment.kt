5c5a0e10389581fa716e10e995b831f916afc6d3
Budi Setiawan <budi.setiawan112001@gmail.com>
2022-06-10 02:07:52 +0700

    Add dating

--- app/src/main/java/com/catpedigree/capstone/catpedigreebase/presentation/ui/dating/DatingFragment.kt
+++ app/src/main/java/com/catpedigree/capstone/catpedigreebase/presentation/ui/dating/DatingFragment.kt
@@ -1,60 +1,115 @@
 package com.catpedigree.capstone.catpedigreebase.presentation.ui.dating
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.catpedigree.capstone.catpedigreebase.R
+import com.catpedigree.capstone.catpedigreebase.data.network.item.UserItems
+import com.catpedigree.capstone.catpedigreebase.databinding.FragmentDatingBinding
+import com.catpedigree.capstone.catpedigreebase.presentation.factory.ViewModelFactory
+import com.google.android.material.snackbar.Snackbar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DatingFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DatingFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private lateinit var _binding: FragmentDatingBinding
+    private val binding get() = _binding
+    private lateinit var user: UserItems
+
+    private val viewModel: DatingViewModel by viewModels {
+        ViewModelFactory.getInstance(requireContext())
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dating, container, false)
+    ): View {
+        _binding = FragmentDatingBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DatingFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DatingFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupAction()
+        setupViewModel()
+        setupMenu()
+        setupNavigation()
+    }
+
+    private fun setupAction() {
+        binding.apply {
+            cardView.setOnClickListener {
+                Snackbar.make(cardView, R.string.development, Snackbar.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun setupViewModel() {
+        viewModel.userItems.observe(viewLifecycleOwner) { userItems ->
+            if (userItems?.isLoggedIn == false) {
+                findNavController().navigateUp()
+            }
+            this.user = userItems
+        }
+    }
+
+    private fun setupMenu(){
+        binding.topAppBar.setOnMenuItemClickListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.profile -> {
+                    findNavController().navigate(R.id.action_datingFragment_to_myProfileFragment)
+                    true
+                }
+                R.id.language -> {
+                    startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+                    true
+                }
+                R.id.account -> {
+                    findNavController().navigate(R.id.action_datingFragment_to_accountFragment)
+                    true
+                }
+                R.id.about -> {
+                    findNavController().navigate(R.id.action_datingFragment_to_aboutFragment)
+                    true
+                }
+                R.id.logout -> {
+                    viewModel.logout()
+                    true
+                }
+                else -> {
+                    false
                 }
             }
+        }
+    }
+
+    private fun setupNavigation() {
+        binding.apply {
+            bottomNavigationView.setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.menu_home -> {
+                        findNavController().navigate(R.id.action_datingFragment_to_homeFragment)
+                        return@setOnItemSelectedListener true
+                    }
+                    R.id.menu_service -> {
+                        findNavController().navigate(R.id.action_datingFragment_to_servicesFragment)
+                        return@setOnItemSelectedListener true
+                    }
+                    R.id.menu_pedigree -> {
+                        findNavController().navigate(R.id.action_datingFragment_to_pedigreeFragment)
+                        return@setOnItemSelectedListener true
+                    }
+                }
+                return@setOnItemSelectedListener false
+            }
+            fab.setOnClickListener {
+                findNavController().navigate(R.id.action_datingFragment_to_resultFragment)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/catpedigree/capstone/catpedigreebase/presentation/ui/dating/DatingViewModel.kt
@@ -0,0 +1,33 @@
+package com.catpedigree.capstone.catpedigreebase.presentation.ui.dating
+
+import androidx.lifecycle.*
+import com.catpedigree.capstone.catpedigreebase.data.local.repository.UserRepository
+import com.catpedigree.capstone.catpedigreebase.utils.error.AuthError
+import kotlinx.coroutines.launch
+
+class DatingViewModel(private val userRepository: UserRepository) : ViewModel() {
+
+    var userItems = userRepository.userItems.asLiveData()
+
+    private val _isSuccess = MutableLiveData<Boolean>()
+    val isSuccess: LiveData<Boolean> = _isSuccess
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _errorMessage = MutableLiveData<String>()
+    val errorMessage: LiveData<String> = _errorMessage
+
+    fun logout() {
+        viewModelScope.launch {
+            _isLoading.value = true
+            try {
+                userRepository.logout()
+            } catch (e: AuthError) {
+                _errorMessage.value = e.message
+            } finally {
+                _isLoading.value = false
+            }
+        }
+    }
+}
